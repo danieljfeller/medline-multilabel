8f70ee69bd798b04bae535d3964efc071252da63
Daniel <danieljfeller@gmail.com>
2018-12-29 13:33:01 -0500

    Initialized repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experiments related to multmultilabel document classification, using the OHSUmed corpus of annotated Medline abstracts. ',
+    author='Daniel Feller',
+    license='MIT',
+)
